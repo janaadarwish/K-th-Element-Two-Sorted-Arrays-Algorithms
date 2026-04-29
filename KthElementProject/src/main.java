3a99c556c3f59ea82480f3b6a714e51aa4d70da0
Nour Ehab <nour.ehab167@gmail.com>
2026-04-29 18:11:56 +0300

    iterative
    ..

--- KthElementProject/src/main.java
+++ KthElementProject/src/main.java
@@ -1,3 +1,22 @@
 public class main {
+    public static void main(String[] args) {
+        
+        Iterative solver1 = new Iterative();
+
+        // Example 1
+        int[] arr1 = {2, 3, 6, 7, 9};
+        int[] arr2 = {1, 4, 8, 10};
+        int k1 = 5;
+
+        // Example 2
+        int[] arr3 = {100, 112, 256, 349, 770};
+        int[] arr4 = {72, 86, 113, 119, 265, 445, 892};
+        int k2 = 7;
+
+        // Output
+        System.out.println("Test Case 1 of non-recursive algorithm (Expected 6): " + solver1.findKth(arr1, arr2, k1));
+        System.out.println("Test Case 2 of non-recursive algorithm (Expected 256): " + solver1.findKth(arr3, arr4, k2));
+    }
+
     
 }
