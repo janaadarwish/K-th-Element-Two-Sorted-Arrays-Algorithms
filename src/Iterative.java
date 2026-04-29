c65426c6a8b27c04ffb6f76f60dc6eabfdd23626
janaadarwish <janaa.dariwsh@gmail.com>
2026-04-29 19:15:35 +0300

    Added main and recursive files

---
+++ src/Iterative.java
@@ -0,0 +1,40 @@
+public class Iterative {
+    public int findKth(int[] arr1, int[] arr2, int k) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int i = 0, j = 0;  
+        int count = 0; 
+        int answer = 0;
+
+        
+        while (i < n1 && j < n2) {
+            if (arr1[i] <= arr2[j]) {
+                answer = arr1[i];
+                i++;
+            } else {
+                answer = arr2[j];
+                j++;
+            }
+            count++;
+            if (count == k) return answer;
+        }
+
+        
+        while (i < n1) {
+            answer = arr1[i];
+            count++;
+            if (count == k) return answer;
+            i++;
+        }
+
+        while (j < n2) {
+            answer = arr2[j];
+            count++;
+            if (count == k) return answer;
+            j++;
+        }
+
+        return answer;
+    }
+    
+}
